309b7c76ccf2cea7eb9cc78b07c640ea1a7be5dd
Yuttasart Viratpan <yuttasartwoodsiam@gmail.com>
2022-03-24 15:31:27 +0700

    Files created

---
+++ src/main/java/muzoo/io/sscsw/webappbackend/NetflixCloneAndChillWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package muzoo.io.sscsw.webappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneAndChillWebappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneAndChillWebappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muzoo/io/sscsw/webappbackend/NetflixCloneAndChillWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package muzoo.io.sscsw.webappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCloneAndChillWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
