903c02d75d0d108e70bf5358796ce9254706c8c0
ThomasHarris0147 <65774362+ThomasHarris0147@users.noreply.github.com>
2022-03-30 17:15:10 +0700

    reset start-mysql.sh start-nginx-vod-local.sh

--- start-nginx-vod-local.sh
+++ start-nginx-vod-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name nginx-vod  -p 127.0.0.1:8082:8080 -v C:/Users/User/docker/videos:/opt/static/videos -v C:/Users/User/docker/config/nginx.conf:/usr/local/nginx/conf/nginx.conf -d nytimes/nginx-vod-module
+docker run --name nginx-vod  -p 127.0.0.1:8082:8080 -v /Users/sai/docker/videos:/opt/static/videos -v /Users/sai/docker/config/nginx.conf:/usr/local/nginx/conf/nginx.conf -d nytimes/nginx-vod-module
