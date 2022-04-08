d7ffa44b2a5f9733bcfdfd4280ac3c877b702578
thanatadcs <thanatad.cs@gmail.com>
2022-04-08 15:01:47 +0700

    Change get videos API to be accessible only by login user

--- src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		// Permit root and /api/login and /api/logout
 		http.authorizeRequests()
-				.antMatchers("/", "/api/login", "/api/whoami", "/api/create", "/api/videos").permitAll();
+				.antMatchers("/", "/api/login", "/api/whoami", "/api/create").permitAll();
 		// permit all OPTIONS requests
 		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 
